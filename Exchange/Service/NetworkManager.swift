85cd0da1b8e81c50203f2f9f81bdd24f98cc4e8b
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-08-24 14:34:10 +0300

    network manager end

--- Exchange/Service/NetworkManager.swift
+++ Exchange/Service/NetworkManager.swift
@@ -7,14 +7,30 @@
 
 import Foundation
 
-
-
 class NetworkManager{
     
     static let shared = NetworkManager()
     private init() {}
     
     func request <T : Decodable > (_ endpoint : EndPoint , completion : @escaping (Result<T , Error> ) -> Void ) -> Void{
-        
+        let urlSessionTask = URLSession.shared.dataTask(with: endpoint.request()) {(data , response , error) in
+            if let error = error{
+                print(error)
+            }
+            if let response = response as? HTTPURLResponse {
+                if (response.statusCode >= 200 && response.statusCode <= 500) {print(response.statusCode)}
+                else{return}
+            }
+            if let data = data {
+                do {
+                    let decodeJsonData = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decodeJsonData))
+                }
+                catch let error{
+                    completion(.failure(error))
+                }
+            }
+        }
+        urlSessionTask.resume()
     }
 }
